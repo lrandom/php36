e5b25e6bd0098ba13da04592004decb7a694174b
root <luann4099@gmail.com>
2019-07-18 19:51:51 +0700

    - add list admin

--- session8/admin/admin/index.php
+++ session8/admin/admin/index.php
@@ -10,17 +10,59 @@
         <div class="content-wrapper">
             <section class="content-header">
                 <h1>
-                    Page Header
-                    <small>Optional description</small>
+                    Quản trị admin
+                    <small>Danh sách admin</small>
                 </h1>
-                <ol class="breadcrumb">
-                    <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-                    <li class="active">Here</li>
-                </ol>
             </section>
             <!-- Main content -->
             <section class="content container-fluid">
+                <?php
+                include_once('./../../dals/admin.php');
+                $obj = new Admin();
+                $rs = $obj->getAll();
+                ?>
+                <div class="box">
+                    <div class="box-header with-border">
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <table class="table table-bordered">
+                            <tbody>
+                                <tr>
+                                    <th style="width: 10px">#</th>
+                                    <th>username</th>
+                                    <th>email</th>
+                                    <th>Fullname</th>
+                                    <th>Thao tác</th>
+                                </tr>
+                                <tr>
+                                    <?php if ($rs != null) {
+                                        foreach ($rs as $r) {
+                                            ?>
+                                    <td><?php echo $r[Admin::COL_ID]; ?></td>
+                                    <td><?php echo $r[Admin::COL_USERNAME]; ?></td>
+                                    <td><?php echo $r[Admin::COL_EMAIL]; ?></td>
+                                    <td><?php echo $r[Admin::COL_FULLNAME]; ?></td>
+                                    <td></td>
+                                    <?php
+                                        }
+                                    } ?>
+                                </tr>
 
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.box-body -->
+                    <div class="box-footer clearfix">
+                        <ul class="pagination pagination-sm no-margin pull-right">
+                            <li><a href="#">«</a></li>
+                            <li><a href="#">1</a></li>
+                            <li><a href="#">2</a></li>
+                            <li><a href="#">3</a></li>
+                            <li><a href="#">»</a></li>
+                        </ul>
+                    </div>
+                </div>
             </section>
         </div>
         <?php include_once('../common/footer.php'); ?>
