5eea333e95beb4addaf0bf16dddd3eed03405400
root <luann4099@gmail.com>
2019-07-18 19:20:05 +0700

    update PDO

---
+++ session8/admin/common/scripts.php
@@ -0,0 +1,10 @@
+<?php
+include_once('../../constant.php');
+?>
+
+<!-- jQuery 3 -->
+<script src="<?php echo BASE_URL ?>assets/adminlte/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo BASE_URL ?>assets/adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo BASE_URL ?>assets/adminlte/dist/js/adminlte.min.js"></script>
